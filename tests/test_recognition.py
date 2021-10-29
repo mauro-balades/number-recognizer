b7c0efe75514323d180c216ee863963c9f37d180
mauro-balades <mauro.balades@tutanota.com>
2021-10-29 18:54:46 +0200

    removed absolute path

--- tests/test_recognition.py
+++ tests/test_recognition.py
@@ -5,5 +5,5 @@ from number_recognition import NumberRecogniser
 
 n = NumberRecogniser()
 n.load()
-num = n.recognize('/home/mauro/work/number-recognition/tests/numbers/5.png')
+num = n.recognize('./numbers/5.png')
 print(num)
