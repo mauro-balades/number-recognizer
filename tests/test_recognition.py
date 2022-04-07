eb58be439c56d2633c654af058af01ddd26dccc5
Mauro Baladés <mauro.balades@tutanota.com>
2022-04-07 16:25:42 +0200

    fixed tests

--- tests/test_recognition.py
+++ tests/test_recognition.py
@@ -1,9 +1,9 @@
 
 
 
-from number_recognition import NumberRecogniser
+from number_recognition import NumberRecognizer
 
-n = NumberRecogniser()
+n = NumberRecognizer()
 n.load()
 num = n.recognize('./numbers/5.png')
 print(num)
